1e4ca3f8e5057e94cef519994932f2342df9e10b
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-07-05 02:33:29 +0500

    File System (Node JS)

---
+++ Class 01/app.js
@@ -0,0 +1,60 @@
+// console.log("Hello World")
+// import fs from "fs"
+
+// const createFile = (() => {
+//     fs.writeFile("readme.md", "#Mustafa", () => {
+
+//     })
+// })()
+
+// // const readFile = (() => {
+// // fs.readFile("./readme.md", "utf-8" , (err, reposnse)=>{
+// // if(err){
+// //     console.log(err , "masla");
+    
+// // }else{
+// //     console.log(reposnse , "file read");
+    
+// // }
+// //     });
+
+// // })()
+
+
+
+// const updateFile = (()=>{
+// fs.appendFileSync("./readme.md", "Shahzad")
+// fs.appendFile("./readme.md", "\n Ahmed" , (err , res)=>{
+// if(err){
+//     console.log(err);
+    
+// }else{
+//     console.log(res);
+    
+// }
+// })
+// })()
+
+// const readUpdatedFile = (()=>{
+// fs.readFile("./readme.md", "utf-8" , (err , res)=>{
+// if(err){
+// console.log(err , `error`);
+
+// }else{
+// console.log(res , "response aagaya");
+
+// }
+// })
+// })()
+
+// const deleteFile = (()=>{
+// fs.unlink("./readme.md" , (err , res)=>{
+// if(err){
+// console.log(err);
+
+// }else{
+// console.log("file delete..");
+
+// }
+// })
+// })()
