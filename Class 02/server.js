ead11924097d462ba532a871df0f02279732d71c
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-07-10 18:15:58 +0500

    url handler and CRUD functionality with user save in file system

--- Class 02/server.js
+++ Class 02/server.js
@@ -1,16 +1,58 @@
-import http from "http"
-const PORT = 4000 
-const server = http.createServer((request , response)=>{
-console.log(request.url);
+import fs from "fs"
 
-if(request.url === "/"){
-response.end("Server running.. on full speed")
-}else if(request.url === "/jobs"){
-response.end("Hiring JOBS..")
-}else if(request.url === "/company"){
-response.end("Aplinode")
-}
+
+import http from "http"
+const PORT = 4000
+const server = http.createServer((request, response) => {
+    console.log(request.url);
+
+    const userData = {}
+
+    if (request.url === "/") {
+        response.end("Server running.. on full speed")
+    } else if (request.url === "/createUser") {
+        // console.log("Hi");
+        // response.end("JSON")
+
+        userData.email = "mustafa@gmail.com"
+        userData.password = 123456
+
+        const createFile = (() => {
+            fs.mkdirSync("./content/data", { recursive: true })
+            fs.writeFileSync("./content/data/userData", JSON.stringify(userData))
+            const readFile = fs.readFileSync("./content/data/userData", "utf-8")
+            // console.log(readFile);
+            response.setHeader("Content-Type", "application/json")
+            response.end(readFile)
+        })()
+
+
+    } else if (request.url === "/updateUser") {
+        const updateFile = (() => {
+            userData.email = "jaffar@gmail.com"
+            userData.password = 123456
+            fs.mkdirSync("./content/data", { recursive: true })
+            fs.writeFileSync("./content/data/userData", JSON.stringify(userData))
+            const readFile = fs.readFileSync("./content/data/userData", "utf-8")
+            // console.log(readFile);
+            response.setHeader("Content-Type", "application/json")
+            response.end(readFile)
+        })()
+    } else if (request.url === "/deleteUser") {
+        fs.mkdirSync("./content/data", { recursive: true })
+        fs.writeFileSync("./content/data/userData", "")
+        const readFile = fs.readFileSync("./content/data/userData", "utf-8")
+        // console.log(readFile);
+        response.setHeader("Content-Type", "application/json")
+        response.end("User Deleted..")
+    }
 
 })
 
-server.listen(PORT , ()=> console.log(`Server running on PORT:${PORT}`))
+server.listen(PORT, () => console.log(`Server running on PORT:${PORT}`))
+
+
+
+
+
+
