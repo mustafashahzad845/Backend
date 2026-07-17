bf56365b60c55f245e2044e9169d54b822ae836f
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-07-17 17:52:31 +0500

    Old Method Server

---
+++ Class 04/server.js
@@ -0,0 +1,20 @@
+// console.log("Hello World");
+
+
+// import http from "http"
+// const PORT = 5400
+
+// const server = http.createServer((request , response)=>{
+// if(request.url === "/"){
+// response.end("Sever running..")
+
+// }else if(request.url === "/job"){
+// response.end("Aplinode hai na..")
+// }else{
+//     response.end(`Invalid Request ${request.url}`)
+// }
+
+// })
+
+
+// server.listen(PORT , ()=>console.log(`Server running on localhost:${PORT}`))
