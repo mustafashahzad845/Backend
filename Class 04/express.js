078c9b8a82d88053ddf12e29be46da4bba376227
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-07-17 18:12:36 +0500

    Express server made and API's

---
+++ Class 04/express.js
@@ -0,0 +1,27 @@
+// import express from "express"
+// const app = express()
+// const PORT = 5500
+
+// app.get("/" , (request , response)=>{
+// response.send("Server chalraha hai nodejs ka express library se")
+// })
+
+// app.get("/jobs" , (request , response)=>{
+// response.send("Aplinode haina")
+// })
+
+// app.post("/create-user" , (request , response)=>{
+// response.send("user-created")
+// })
+
+// app.put("/update-user" , (request , response)=>{
+// response.send("User updated")
+// })
+
+// app.delete("/delete-user" , (request , response)=>{
+// response.send("User deleted")
+// })
+
+// app.listen(PORT , ()=>console.log(`Nodejs Sevrer runnig using express library`)
+// )
+
