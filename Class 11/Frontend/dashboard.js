c9298256c7cd387c3847cc6df6f44f6b955f1a1c
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-08-07 15:17:31 +0500

    Previous class practice

---
+++ Class 11/Frontend/dashboard.js
@@ -0,0 +1,19 @@
+const validateUser = (() =>{
+    const userId = localStorage.getItem("user")
+    console.log(userId);
+    
+
+if(!userId){
+// user login nhi hai
+location.replace("./index.html")
+return
+}
+
+})()
+
+
+const logoutFunctionality = () => {
+    localStorage.clear()
+    location.replace("./index.html")
+}
+
